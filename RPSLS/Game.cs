9a620e924ff5ca7068a716b153febee87088300f
Key Admin <mdepeiza@keyconceptskb.com>
2021-01-14 14:58:32 -0500

    Initial commit.

---
+++ RPSLS/Game.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    class Game
+    {
+        //member variables
+        Player playerOne;
+        Player playerTwo;
+
+        //constructor
+        public Game()
+        {
+            //if user picks single player create human and cpu
+            //else make two humans
+        }
+
+        //member methods
+        public void DisplayRules() //change to fleshed out method
+        {
+
+        }
+    }
+}
