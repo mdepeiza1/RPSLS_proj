9d37549ba06a0ab55cf164f5e2037d4a14f1256b
Key Admin <mdepeiza@keyconceptskb.com>
2021-01-14 15:20:47 -0500

    Added logic to display rules and choose single or multiplayer to the Game class.

--- RPSLS/Game.cs
+++ RPSLS/Game.cs
@@ -17,12 +17,54 @@ namespace RPSLS
         {
             //if user picks single player create human and cpu
             //else make two humans
+            DisplayRules();
+            ChooseSingleOrMultiPlayer();
         }
 
         //member methods
         public void DisplayRules() //change to fleshed out method
         {
-
+            Console.WriteLine("This is a game of Rock, Paper, Scissors, Lizard, Spock. " +
+                "Rock beats Scissors. " +
+                "Paper beats Rock. " +
+                "Scissors beats Paper. " +
+                "Rock beats Lizard. " +
+                "Paper beats Spock. " +
+                "Scissors beats Lizard. " +
+                "Lizard beats Spock. " +
+                "Spock beats Scissors. " +
+                "Lizard beats Paper. " +
+                "Spock beats Rock. " +
+                "Press enter to continue.");
+            Console.ReadLine();
+        }
+        public int ChooseSingleOrMultiPlayer()
+        {
+            int result1;
+            Console.WriteLine("Please enter 1 for a single " +
+                "player game, and 2 for a multiplayer game.");
+            while (true)
+            {
+                if (Int32.TryParse(Console.ReadLine(), out int result2))
+                {
+                    if ((result2 == 1) || (result2 == 2))
+                    {
+                        result1 = result2;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your input was not 1 or 2. " +
+                        "Please enter either 1 or 2.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Your input was not an integer. " +
+                        "Please enter an integer.");
+                }
+            }
+            return result1;
         }
     }
 }
